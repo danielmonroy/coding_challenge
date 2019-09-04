d6b56e47870bea5f02857748c0d90792b360b089
Daniel Monroy <danielmonroy@ciencias.unam.mx>
2019-09-04 01:31:10 -0500

    Readu simplify function

--- fractions.rb
+++ fractions.rb
@@ -1,7 +1,8 @@
 ##
 # In this program I will represent fractions as an array
 # of two integers, where the first element is the numerator
-# and the second is the denominator
+# and the second is the denominator.
+
 
 ##
 # Function to convert strings in the form [Integer_]Numerator/Denominator to my
@@ -60,19 +61,33 @@ def divide(fraction_1, fraction_2)
 end
 
 ##
-# Function to get the lowest possible denominator using the
+# Function to get the lowest possible denominator, searching for the highest
+# common divisor
 def simplify_function(f)
+  iterator = 1
+  maximum_divisible_number = 1
+  while iterator <= f.last
+    if f.first % iterator == 0 and f.last % iterator == 0
+      maximum_divisible_number = iterator
+    end
+    iterator += 1
+  end
+  return [(f.first / maximum_divisible_number), (f.last / maximum_divisible_number)]
 end
 
+##
+# Function to convert my representation of fractions in the desired string
+# representation
 def fraction_to_string(f)
-  if f.first.abs > f.last
+  f = simplify_function(f)
+  if f.first.abs >= f.last
     if f.first % f.last == 0
       return (f.first / f.last).to_s
     else
       return (f.first / f.last).to_s + "_" + (f.first % f.last).to_s + "/" + f.last.to_s
     end
   else
-    return (f.first.to_s + "/" + f.last.to_s)
+    f.last == 1 ? (return f.first.to_s) : (return (f.first.to_s + "/" + f.last.to_s))
   end
 end
 
